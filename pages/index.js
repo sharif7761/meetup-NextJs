95ec34241e666ec8ab67c30772ee9b88b1e65ea5
Puspita Vabna <sharif7761@gmail.com>
2022-02-06 23:23:13 +0600

    make some dummy meetup data

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+const DUMMY_MEETUPS = [
+    {
+        id: 'm1',
+        title: "A First Meetup",
+        image: 'https://picsum.photos/200/300',
+        address: 'Uttara, Dhaka',
+        description: 'This is first meetup'
+    },
+    {
+        id: 'm2',
+        title: "A Second Meetup",
+        image: 'https://picsum.photos/200/300',
+        address: 'Uttara, Dhaka',
+        description: 'This is Second meetup'
+    },
+    {
+        id: 'm3',
+        title: "A Third Meetup",
+        image: 'https://picsum.photos/200/300',
+        address: 'Uttara, Dhaka',
+        description: 'This is Third meetup'
+    },
+]
+
+
+function HomePage() {
+    return (
+        <>
+        </>
+    )
+}
+
+export default HomePage;
