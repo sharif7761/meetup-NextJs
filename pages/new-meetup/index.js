219ff0c97c231ad6d4593d4631c93ceb97d11fb3
Puspita Vabna <sharif7761@gmail.com>
2022-02-08 01:26:38 +0600

    new meetup page

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,11 @@
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
+function NewMeetupPage(){
+    function addMeetupHandler(entereMeetupData){
+        console.log(entereMeetupData);   
+    }
+
+    return <NewMeetupForm onAddMeetup={addMeetupHandler} />
+}
+
+export default NewMeetupPage;
