2c5621385d7aaa7314b8c9091e9889596c665368
Puspita Vabna <sharif7761@gmail.com>
2022-02-11 23:18:28 +0600

    meetup details page

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+function MeetupDetails(){
+    return (
+        <>
+            <img src="https://picsum.photos/200/300" alt="meetup img" />
+            <h1>A First Meetup</h1>
+            <address>Uttara, Dhaka</address>
+            <p>The meetup description</p>
+        </>
+    )
+}
+
+export default MeetupDetails;
